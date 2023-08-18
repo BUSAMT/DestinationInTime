16d51e9ad2fb7eb3a8d012f1a60a4b59eb33ee22
staceyjf <staceyfanner@gmail.com>
2023-08-18 17:31:13 +1000

    urls.py adds for sign up

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # Itineraries (/itins/)
+
     # user -  /accounts
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
 
