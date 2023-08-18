f90deddbd579983ca10205eb17b9ffaae4284b49
staceyjf <staceyfanner@gmail.com>
2023-08-18 16:39:27 +1000

    initial auth adds excluding model link

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    # user -  /accounts
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
