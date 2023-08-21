303f5fabad5d39a269d98c982ab56a98b700f275
OsoSan01 <gamaliel.l.l@hotmail.com>
2023-08-21 17:32:27 +1000

    Updated model with new names

--- main_app/models.py
+++ main_app/models.py
@@ -6,11 +6,13 @@ from django.contrib.auth.models import User
 
 # Destination model
 class Destination(models.Model):
-    dest_name = models.CharField(max_length=150)
-    dest_description = models.TextField(max_length=3000)
-    dest_address = models.CharField(max_length=300)
-    dest_country = models.CharField(max_length=100)
-    price_range = models.CharField(max_length=100)
+    dest_name = models.CharField("Name",max_length=150)
+    dest_description = models.TextField("Description",max_length=3000)
+    dest_address = models.CharField("Address",max_length=300)
+    dest_latitude = models.IntegerField("Latitude")
+    dest_longitude = models.IntegerField("Longitude")
+    dest_country = models.CharField("Country",max_length=100)
+    price_range = models.CharField("Price Range", max_length=100)
 
     def __str__(self):
         return self.dest_name
@@ -21,11 +23,11 @@ class Destination(models.Model):
 
 # Era model
 class Era(models.Model):
-    era_name = models.CharField(max_length=250)
-    era_peroid = models.CharField(max_length=200)
-    era_description = models.TextField(max_length=2000)
+    era_name = models.CharField("Name", max_length=250)
+    era_period = models.CharField("Period", max_length=200)
+    era_description = models.TextField("Description", max_length=2000)
     # M:M with Era
-    destination = models.ManyToManyField(Destination)
+    destinations = models.ManyToManyField(Destination)
 
     def __str__(self):
         return self.era_name
@@ -33,10 +35,11 @@ class Era(models.Model):
 
 #  Itinerary model 
 class Itinerary(models.Model):
-    init_travel_date = models.DateField("Starting journey date")
-    end_travel_date = models.DateField("Ending journey date")
-    user_budget = models.IntegerField()
-    itin_description = models.TextField(max_length=3000)
+    init_name = models.CharField("Name", max_length=100)
+    itin_description = models.TextField("Description", max_length=1000)
+    init_travel_date = models.DateField("Start Date")
+    end_travel_date = models.DateField("End Date")
+    user_budget = models.IntegerField("Budget")
 # user model built within django
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -50,11 +53,11 @@ class Itinerary(models.Model):
 
 # Chosen destination model
 class Stop(models.Model):
-    dest_name = models.CharField(max_length=150)
-    dest_description = models.CharField(max_length=3000)
-    init_date_at_dest = models.DateField('Starting date at destination')
-    end_date_at_dest = models.DateField('Ending date at destination')
-    comments = models.TextField(max_length=3000)
+    dest_name = models.CharField("Name", max_length=150)
+    dest_description = models.CharField("Description", max_length=3000)
+    init_date_at_dest = models.DateTimeField("Start")
+    end_date_at_dest = models.DateTimeField("End")
+    comments = models.TextField("Comments", max_length=3000)
 # O:M with destination
     destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='stop_destin')
 # O:M with itinerary
